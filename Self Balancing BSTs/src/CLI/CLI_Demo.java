16d5026b1625decf963fc6f2425962dcacf1eaf2
Mkario Michel <96423846+Mkario@users.noreply.github.com>
2023-03-21 18:47:05 +0200

    All codes merged

--- Self Balancing BSTs/src/CLI/CLI_Demo.java
+++ Self Balancing BSTs/src/CLI/CLI_Demo.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 public class CLI_Demo {
 
-    public Super_Tree<String> myTree;
+    public Super_Tree<String> myDictionary;
 
     public void WrongMessage() {
         System.out.println("Wrong Command");
@@ -19,6 +19,10 @@ public class CLI_Demo {
         while(true) {
             input = scanner.nextLine();
             String[] args = input.split(" ");
+            if(!args[0].equals("new") && !args[0].equals("exit") && myDictionary == null) {
+                System.out.println("no dictionary is created yet");
+                break;
+            }
             switch (args[0]) {
                 case "exit":
                     System.exit(0);
@@ -28,16 +32,16 @@ public class CLI_Demo {
                         break;
                     }
                     if(args[1].equalsIgnoreCase("AVL"))
-                        myTree = new AVL_Tree<String>();
+                        myDictionary = new AVL_Tree<String>();
                     else if(args[1].equalsIgnoreCase("RB"))
-                        myTree = new RB_Tree<String>();
+                        myDictionary = new RB_Tree<String>();
                     break;
                 case "insert":
                     if(args[1] == null) {
                         WrongMessage();
                         break;
                     }
-                    if(myTree.insert(args[1]))
+                    if(myDictionary.insert(args[1]))
                         System.out.println(args[1] + " was inserted successfully");
                     break;
                 case "delete":
@@ -45,7 +49,7 @@ public class CLI_Demo {
                         WrongMessage();
                         break;
                     }
-                    if(myTree.delete(args[1]))
+                    if(myDictionary.delete(args[1]))
                         System.out.println(args[1] + " was deleted successfully");
                     break;
                 case "search":
@@ -53,7 +57,7 @@ public class CLI_Demo {
                         WrongMessage();
                         break;
                     }
-                    if(myTree.search(args[1]))
+                    if(myDictionary.search(args[1]))
                         System.out.println(args[1] + " was found successfully");
                     break;
                 case "batch":
@@ -62,22 +66,24 @@ public class CLI_Demo {
                         break;
                     }
                     if(args[1].equals("insert"))
-                        myTree.batchInsert(args[2]);
+                        myDictionary.batchInsert(args[2]);
                     else if(args[1].equals("delete"))
-                        myTree.batchDelete(args[2]);
+                        myDictionary.batchDelete(args[2]);
                     else WrongMessage();
                     break;
                 case "size":
-                    System.out.println(myTree.getSize());
+                    System.out.println(myDictionary.getSize());
                     break;
                 case "height":
-                    System.out.println(myTree.getHeight());
+                    System.out.println(myDictionary.getHeight());
                     break;
                 default:
                     WrongMessage();
             }
         }
     }
+
+
 
     public static void main(String[] args) {
         CLI_Demo CLI = new CLI_Demo();
